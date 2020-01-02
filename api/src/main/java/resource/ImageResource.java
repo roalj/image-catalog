b7a83f457f100a4b52c9cac8cb02c25cba40af34
roalj <roko4059@student.uni-lj.si>
2020-01-02 18:30:34 +0100

    Comment out logs, free trial ended

--- api/src/main/java/resource/ImageResource.java
+++ api/src/main/java/resource/ImageResource.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @ApplicationScoped
 @Path("/images")
-@Log
+//@Log
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ImageResource {
