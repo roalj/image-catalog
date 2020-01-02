e9d95c4ce33e0d5a20d040689f8cd3d8a36be8a0
roalj <roko4059@student.uni-lj.si>
2020-01-02 18:31:16 +0100

    Add async -not working

--- api/src/main/java/Application.java
+++ api/src/main/java/Application.java
@@ -1,8 +1,15 @@
 import com.kumuluz.ee.discovery.annotations.RegisterService;
+import resource.ImageResource;
 
 import javax.ws.rs.ApplicationPath;
+import java.util.Collections;
+import java.util.Set;
 
 @RegisterService
 @ApplicationPath("api")
 public class Application extends javax.ws.rs.core.Application {
+    @Override
+    public Set<Class<?>> getClasses() {
+        return Collections.singleton(ImageResource.class);
+    }
 }
